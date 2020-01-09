f152d14a35bb10b67e1a62f19b669ecc7236c6bd
Vlad Gerasimovich <vlad.gerasimovich.dev@gmail.com>
2020-01-09 16:39:18 +0300

    add gulp tasker

---
+++ README.md
@@ -0,0 +1,27 @@
+# AccEss: Accessible Essentials
+
+### Content
+
+**[What is this?](#what-is-this)**  
+**[Roadmap](#roadmap)**  
+**[License](#license)**
+
+## What is this?
+
+WORK IN PROGRESS!
+
+_AccEss_ is a lightweight pack of accessible customizable elements for web development such as Select, Tabs, Checkboxes, etc. It built on HTML5 basic elements and behave itself the exact same way they do.
+
+## Roadmap
+- Select (single option)
+- Select (multiple options)
+- Tabs
+- Checkboxes
+- Radio buttons
+- TBD
+
+## License
+
+Copyright © 2020.
+
+Licensed under the MIT license.
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+
+global.$ = {
+  gulp: require('gulp'),
+  gp: require('gulp-load-plugins')(),
+  config: require('./gulp/configs/config'),
+  path: {
+    tasks: require('./gulp/configs/tasks.js'),
+  },
+
+  browserSync: require('browser-sync').create(),
+  del: require('del'),
+  exec: require('child_process').exec,
+  fs: require('fs'),
+  merge: require('merge-stream'),
+};
+
+$.path.tasks.forEach(function(taskPath) {
+  require(taskPath)();
+});
+
+$.gulp.task(
+  'core',
+  $.gulp.parallel(
+    'pug',
+    'css:vendor',
+    'sass',
+    'js:vendor',
+    // 'js:app',
+    'js:app-minify',
+    'fonts',
+    'images'
+  )
+);
+
+$.gulp.task(
+  'default',
+  $.gulp.series(
+    'clean',
+    'js:lint',
+    'sass:lint',
+    'images:minify',
+    'core',
+    $.gulp.parallel('watch', 'serve')
+  )
+);
+
+$.gulp.task('build', $.gulp.series('clean', 'images:minify', 'core'));
